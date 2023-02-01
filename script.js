84891dccb079e5c4588e1094d04e79e97f8f4112
Pavel <pavelfedorov929@gmail.com>
2023-02-01 17:21:21 +0300

    create phone number kata 6 kuy

---
+++ script.js
@@ -0,0 +1,13 @@
+function createPhoneNumber(numbers) {
+    return numbers.map((num, i) => {
+        if (i === 0) return `(${num}`;
+        if (i === 2) return `${num}) `;
+        if (i === 5) return `${num}-`;
+
+        return num;
+    }).join('');
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // (123) 456-7890
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // (111) 111-1111
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // (123) 456-7890
